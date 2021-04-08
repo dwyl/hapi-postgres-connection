b4f8ee3c8d1c4a7026f5246f9d136de1b0b28fe5
Hector del Rio <hector-del-rio@users.noreply.github.com>
2021-04-08 12:00:13 +0300

    Fix reading DATABASE_SSL env

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ let run_once = false;
 // create a pool
 const pool = new pg.Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: process.env.DATABASE_SSL || false
+  ssl: process.env.DATABASE_SSL.toLowerCase() === "true" || false
 });
 
 const createPoolConnection = async () => {
